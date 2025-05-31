e0ce2757a8a592c16f323e53030ce51f0b60add4
Manuel Rodriguez <cuentapersonal.0098@gmail.com>
2025-05-30 19:53:59 -0500

    First commit

---
+++ src/index.ts
@@ -0,0 +1,78 @@
+import fs from 'fs'
+
+let base:number = 1;
+let header:string = `
+===================
+    Tabla del ${base}    
+===================
+`;
+let contentTable:string = ``;
+
+
+while ( base <= 10 ) {
+    
+    for (let i = 1; i <= 10; i++) {
+    
+        const result = base * i;
+header = `
+===================
+    Tabla del ${base}      
+===================
+`;
+        contentTable += `     ${base} x ${i} = ${result}\n`;
+
+        if( i === 10 ){
+        
+            console.log(header+contentTable);
+
+    if(!fs.existsSync('ouputs') ){
+
+        fs.mkdir('ouputs', { recursive: true }, (err)=>{ if(err) throw err })
+        console.log('Se creo la carpeta ouputs');
+
+    }if ( fs.existsSync('ouputs') ){
+        console.log('Ya existe esa carpeta');
+        fs.appendFileSync(`./ouputs/table del ${base}.txt`, header+contentTable )
+    }
+
+            base = base + 1
+            i = 1
+            contentTable = ``;
+        
+        break
+    } 
+        
+    }    
+
+}
+
+// console.log(contentTable);
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+    
+//     const result = base * i;
+
+//     contentTable += `     ${base} x ${i} = ${result}\n`
+    
+// }
+
+
+
+
+// console.log(header+contentTable);
+
+
+// if(!fs.existsSync('ouputs') ){
+
+    
+//     fs.mkdir('ouputs', { recursive: true }, (err)=>{ if(err) throw err })
+//     console.log('Se creo la carpeta ouputs');
+
+// }if ( fs.existsSync('ouputs') ){
+//     console.log('Ya existe esa carpeta');
+//     fs.appendFileSync('./ouputs/table del 5.txt', tableMultiplication )
+// }
+
